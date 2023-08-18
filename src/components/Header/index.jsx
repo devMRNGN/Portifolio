919677e2b084d9d66dfc80d9abd80cf70457887e
João Marangoni <joao.tadeuzi00@gmail.com>
2023-08-17 22:20:13 -0300

    Mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,27 @@
 import { useEffect, useState } from 'react';
 import logo from '../../assets/PortifolioImages/logo.png';
+import mobileLogo from '../../assets/PortifolioImages/menu-icon-mobile.png';
 import './index.css';
 
 const Header = () => {
   const [buttonSelected, setButtonSelected] = useState('header-button-portifolio');
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    checkScreenWidth();
+
+    // Adicione um ouvinte para o evento de redimensionamento da tela
+    window.addEventListener('resize', checkScreenWidth);
+
+    // Remova o ouvinte quando o componente for desmontado
+      return () => {
+        window.removeEventListener('resize', checkScreenWidth);
+      };
+  }, []);
 
   const handleLinkClick = (event, targetId) => {
     event.preventDefault();
@@ -36,63 +54,79 @@ const Header = () => {
   }, []);
 
   return (
-    <header id="header-div">
-      <nav id="header-nav">
-        <a id="header-logo" href="#sub-container">
-          <img id="logo-style" src={logo} alt="Uma logo escrito DM" />
-        </a>
-        <div id="header-ways">
-          <a
-            id="header-button-portifolio"
-            href="#sub-container"
-            className={`header-links ${buttonSelected === 'sub-container' ? 'active' : ''}`}
-            onClick={(e) => handleLinkClick(e, 'sub-container')}
-            style={{
-              backgroundColor: buttonSelected === 'sub-container' ? '#3A3B3C' : 'hsl(0, 0%, 18.82%)',
-              color: buttonSelected === 'sub-container' ? '#00fff2' : '#E4E6EB',
-            }}
-          >
-            PORTIFÓLIO
-          </a>
-          <a
-            id="header-button-sobre"
-            href="#sobre-container"
-            className={`header-links ${buttonSelected === 'sobre-container' ? 'active' : ''}`}
-            onClick={(e) => handleLinkClick(e, 'sobre-container')}
-            style={{
-              backgroundColor: buttonSelected === 'sobre-container' ? '#3A3B3C' : 'hsl(0, 0%, 18.82%)',
-              color: buttonSelected === 'sobre-container' ? '#00fff2' : '#E4E6EB',
-            }}
-          >
-            SOBRE
-          </a>
-          <a
-            id="header-button-projetos"
-            href="#projeto-div"
-            className={`header-links ${buttonSelected === 'projeto-div' ? 'active' : ''}`}
-            onClick={(e) => handleLinkClick(e, 'projeto-div')}
-            style={{
-              backgroundColor: buttonSelected === 'projeto-div' ? '#3A3B3C' : 'hsl(0, 0%, 18.82%)',
-              color: buttonSelected === 'projeto-div' ? '#00fff2' : '#E4E6EB',
-            }}
-          >
-            PROJETOS
-          </a>
-          <a
-            id="header-button-contato"
-            href="#container-contate"
-            className={`header-links ${buttonSelected === 'container-contate' ? 'active' : ''}`}
-            onClick={(e) => handleLinkClick(e, 'container-contate')}
-            style={{
-              backgroundColor: buttonSelected === 'container-contate' ? '#3A3B3C' : 'hsl(0, 0%, 18.82%)',
-              color: buttonSelected === 'container-contate' ? '#00fff2' : '#E4E6EB',
-            }}
-          >
-            CONTATO
-          </a>
-        </div>
-      </nav>
-    </header>
+    <div id="master-container-header">
+      {
+        isMobile ? 
+          <div id="header-mobile-div">
+            <div id="header-mobile-container">
+              <button>
+                <img src={mobileLogo} alt="" />
+              </button>
+            </div>
+            <a id="header-logo" href="#sub-container">
+                <img id="logo-style" src={logo} alt="Uma logo escrito DM" />
+            </a>
+          </div>
+        :
+        <header id="header-div">
+          <nav id="header-nav">
+            <a id="header-logo" href="#sub-container">
+              <img id="logo-style" src={logo} alt="Uma logo escrito DM" />
+            </a>
+            <div id="header-ways">
+              <a
+                id="header-button-portifolio"
+                href="#sub-container"
+                className={`header-links ${buttonSelected === 'sub-container' ? 'active' : ''}`}
+                onClick={(e) => handleLinkClick(e, 'sub-container')}
+                style={{
+                  backgroundColor: buttonSelected === 'sub-container' ? '#3A3B3C' : 'hsl(0, 0%, 18.82%)',
+                  color: buttonSelected === 'sub-container' ? '#00fff2' : '#E4E6EB',
+                }}
+              >
+                PORTIFÓLIO
+              </a>
+              <a
+                id="header-button-sobre"
+                href="#sobre-container"
+                className={`header-links ${buttonSelected === 'sobre-container' ? 'active' : ''}`}
+                onClick={(e) => handleLinkClick(e, 'sobre-container')}
+                style={{
+                  backgroundColor: buttonSelected === 'sobre-container' ? '#3A3B3C' : 'hsl(0, 0%, 18.82%)',
+                  color: buttonSelected === 'sobre-container' ? '#00fff2' : '#E4E6EB',
+                }}
+              >
+                SOBRE
+              </a>
+              <a
+                id="header-button-projetos"
+                href="#projeto-div"
+                className={`header-links ${buttonSelected === 'projeto-div' ? 'active' : ''}`}
+                onClick={(e) => handleLinkClick(e, 'projeto-div')}
+                style={{
+                  backgroundColor: buttonSelected === 'projeto-div' ? '#3A3B3C' : 'hsl(0, 0%, 18.82%)',
+                  color: buttonSelected === 'projeto-div' ? '#00fff2' : '#E4E6EB',
+                }}
+              >
+                PROJETOS
+              </a>
+              <a
+                id="header-button-contato"
+                href="#container-contate"
+                className={`header-links ${buttonSelected === 'container-contate' ? 'active' : ''}`}
+                onClick={(e) => handleLinkClick(e, 'container-contate')}
+                style={{
+                  backgroundColor: buttonSelected === 'container-contate' ? '#3A3B3C' : 'hsl(0, 0%, 18.82%)',
+                  color: buttonSelected === 'container-contate' ? '#00fff2' : '#E4E6EB',
+                }}
+              >
+                CONTATO
+              </a>
+            </div>
+          </nav>
+        </header>
+      }
+    </div>
   );
 };
 
